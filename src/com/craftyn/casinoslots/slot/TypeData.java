fc197df9d44dc7bdd9104360220acee6513fdba2
graywolf336 <graywolf336@craftyn.com>
2012-03-27 23:37:31 -0500

    Change to display the currency, not the name of the plugin

--- src/com/craftyn/casinoslots/slot/TypeData.java
+++ src/com/craftyn/casinoslots/slot/TypeData.java
@@ -161,7 +161,7 @@ public class TypeData {
 		for(Map.Entry<String, String> entry : messages.entrySet()) {
 			String message = entry.getValue();
 			String key = entry.getKey();
-			message = message.replaceAll("\\[cost\\]", ""+ cost + " " + plugin.economy.getName());
+			message = message.replaceAll("\\[cost\\]", ""+ cost + " " + plugin.economy.currencyNamePlural());
 			messages.put(key, message);
 		}
 		

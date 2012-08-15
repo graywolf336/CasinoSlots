0db80e9944a0ce241a12bf9287d426f09ec535ac
graywolf336 <graywolf336@craftyn.com>
2012-08-15 02:02:27 -0500

    Added another command to '/casino set' and updated the Readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-CasinoSlots
-==========
+# CasinoSlots #
 
 An updated version of AnCasino started by Darazo
+
+### Updates ###
+* Added /casino set sign <slot>
+** This makes it so  you can punch and sign and that sign will update with the latest winner
+* Added /casino set type <slotname> <type>
+** This allows you to change the type of a slot machine to something different
+* Enhanced the way the PlayerListner is handled
+* Made the plugin check the chunks for both the controller and one of the reel blocks
+* Started working on getting managed slots able to be made with permissions
+* Created a check when loading for the seemingly random NPE on getBlock, tells you what to do now
--- src/com/craftyn/casinoslots/command/CasinoSet.java
+++ src/com/craftyn/casinoslots/command/CasinoSet.java
@@ -29,15 +29,42 @@ public class CasinoSet extends AnCommand {
 			}else {
 				// Incorrect command format
 				sendMessage("Usage:");
-				sendMessage("/casino set sign <slotname>");
+				sendMessage("  /casino set sign <slotname>");
+			}
+		}else if(args.length == 4) {
+			if (args[1].equalsIgnoreCase("type")) {
+				
+				// Slot exists
+				if(plugin.slotData.isSlot(args[2])) {
+					if(plugin.typeData.isType(args[3])) {
+						SlotMachine slot = plugin.slotData.getSlot(args[2]);
+						String typeName = args[3];
+						
+						String oldType = slot.getType();
+						slot.setType(typeName);
+						sendMessage("Type successfully changed from '" + oldType + "' to '" + typeName + "'.");
+						
+					}else {
+						// Type does not exist
+						sendMessage("Invalid type of a slot machine.");
+					}
+				}else {
+					// Slot does not exist
+					sendMessage("Invalid slot machine.");
+				}
+			}else {
+				// Incorrect command format
+				sendMessage("Usage:");
+				sendMessage("  /casino set type <slotname> <type>");
 			}
 		}
 		
 		// Incorrect command format
 		else {
 			sendMessage("Usage:");
-			//                    0   1      2
-			sendMessage("/casino set sign <slotname>");
+			//                      0   1      2          3
+			sendMessage("  /casino set sign <slotname>");
+			sendMessage("  /casino set type <slotname> <type>");
 		}
 		
 		return true;

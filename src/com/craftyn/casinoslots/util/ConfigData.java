0d136edf11bdda585301c40394962b877e5fcc74
graywolf336 <graywolf336@craftyn.com>
2012-08-24 00:08:07 -0500

    Make the debug value change when config is reloaded, realized this area has some work to do.

--- src/com/craftyn/casinoslots/util/ConfigData.java
+++ src/com/craftyn/casinoslots/util/ConfigData.java
@@ -80,6 +80,7 @@ public class ConfigData {
 		
 		configFile = new File(plugin.getDataFolder(), "config.yml");
 		config = YamlConfiguration.loadConfiguration(configFile);
+		this.debug = config.getBoolean("options.debug", false);
 		
 		//Set it to null, then reload it
 		slots = null;

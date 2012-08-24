41f7aa573179488f75b39a08db191879e0d4385f
graywolf336 <graywolf336@craftyn.com>
2012-08-24 00:16:28 -0500

    Keep working on the debug, maybe I stupidly set this and thats why it isn't working.

--- src/com/craftyn/casinoslots/util/ConfigData.java
+++ src/com/craftyn/casinoslots/util/ConfigData.java
@@ -98,7 +98,7 @@ public class ConfigData {
 		this.chatColor = config.getString("options.chat.chat-color", "&a");
 		this.displayPrefix = config.getBoolean("options.chat.display-plugin-prefix", true);
 		
-		this.debug = config.getBoolean("options.debug", false);
+		this.debug = config.getBoolean("options.debug");
 		this.trackStats = config.getBoolean("options.track-statistics", true);
 		this.allowDiagonals = config.getBoolean("options.allow-diagonal-winnings", false);
 		this.protection = config.getBoolean("options.enable-slot-protection", true);

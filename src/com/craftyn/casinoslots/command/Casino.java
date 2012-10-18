163f943f148b57e7fa9d9914d722e8006a26a35e
graywolf336 <graywolf336@craftyn.com>
2012-10-18 00:35:28 -0500

    Update the /casino command for the console

--- src/com/craftyn/casinoslots/command/Casino.java
+++ src/com/craftyn/casinoslots/command/Casino.java
@@ -17,6 +17,7 @@ public class Casino extends AnCommand {
 			senderSendMessage("  /casino list - List slot machines and types");
 			senderSendMessage("  /casino reload - Reload config files from disk");
 			senderSendMessage("  /casino stats - Global usage statistics");
+			senderSendMessage("  /casino version - To get the current version of the plugin.");
 			return true;
 		}
 		

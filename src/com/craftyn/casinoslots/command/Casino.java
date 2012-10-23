309773fcdd6b821fee2c9253fcc731bb0c07c45e
graywolf336 <graywolf336@craftyn.com>
2012-10-22 21:07:52 -0500

    Slots is never registered in the Executor

--- src/com/craftyn/casinoslots/command/Casino.java
+++ src/com/craftyn/casinoslots/command/Casino.java
@@ -33,7 +33,6 @@ public class Casino extends AnCommand {
 			sendMessage("  /casino remove - Remove an existing slot machine");
 			sendMessage("  /casino set - Enables various options to set");
 			sendMessage("  /casino setowner - Sets the owner of a managed slot");
-			sendMessage("  /casino slot - Manage slot machines");
 			sendMessage("  /casino stats - Global usage statistics");
 			sendMessage("  /casino toggle - Toggles the state of a slot");
 			sendMessage("  /casino type - Manage slot machine types");
@@ -49,7 +48,6 @@ public class Casino extends AnCommand {
 			sendMessage("  /casino deposit - Deposit money to a managed slot");
 			sendMessage("  /casino list - List slot machines and types");
 			sendMessage("  /casino setowner - Sets the owner of a managed slot");
-			sendMessage("  /casino slot - Manage slot machines");
 			sendMessage("  /casino remove - Remove an existing slot machine");
 			sendMessage("  /casino withdraw - Withdraw money from a managed slot");
 		}

b64f7ba12d20557f435a0de6eb56daa10ba16fe4
graywolf336 <graywolf336@craftyn.com>
2015-01-26 03:31:10 -0600

    Fix the spacing on the StatData.

--- src/main/java/com/craftyn/casinoslots/util/StatData.java
+++ src/main/java/com/craftyn/casinoslots/util/StatData.java
@@ -25,10 +25,11 @@ public class StatData {
 
     // Returns collection of all stats
     public Collection<Stat> getStats() {
-        if(!this.stats.isEmpty()) {
+        if (!this.stats.isEmpty()) {
             return this.stats.values();
+        } else {
+            return null;
         }
-        else return null;
     }
 
     // Returns stats of a type
@@ -46,57 +47,57 @@ public class StatData {
         this.stats.put(stat.getType(), stat);
         calculateGlobal();
     }
-    
-    //Recalculate the global stats
+
+    // Recalculate the global stats
     private void calculateGlobal() {
-    	this.globalSpins = 0;
-    	this.globalWins = 0;
+        this.globalSpins = 0;
+        this.globalWins = 0;
         this.globalLosts = 0;
-        this.globalWon = (double) 0;
-        this.globalLost = (double) 0;
-        
-    	for(Stat s : this.stats.values()) {
-        	this.globalSpins += s.getSpins();
-        	this.globalWins += s.getWins();
+        this.globalWon = 0;
+        this.globalLost = 0;
+
+        for (Stat s : this.stats.values()) {
+            this.globalSpins += s.getSpins();
+            this.globalWins += s.getWins();
             this.globalLosts += s.getLosts();
             this.globalWon += s.getWon();
             this.globalLost += s.getLost();
-    	}
+        }
     }
 
     // Loads a stat
     private void loadStat(String type) {
-        String path = "types." + type +".";
+        String path = "types." + type + ".";
 
         Integer spins = plugin.configData.stats.getInt(path + "spins", 0);
         Integer wins = plugin.configData.stats.getInt(path + "wins", 0);
         Integer losts = plugin.configData.stats.getInt(path + "losts", 0);
         Double won = plugin.configData.stats.getDouble(path + "won", 0);
         Double lost = plugin.configData.stats.getDouble(path + "lost", 0);
-        
+
         this.stats.put(type, new Stat(type, spins, wins, losts, won, lost));
     }
 
     // Load all stats
     public void loadStats() {
-        if(plugin.configData.trackStats) {
+        if (plugin.configData.trackStats) {
             this.globalSpins = 0;
             this.globalWins = 0;
             this.globalLosts = 0;
             this.globalWon = 0.0;
             this.globalLost = 0.0;
 
-            if(plugin.configData.stats.isConfigurationSection("types")) {
+            if (plugin.configData.stats.isConfigurationSection("types")) {
                 Set<String> types = plugin.configData.stats.getConfigurationSection("types").getKeys(false);
-                for(String type : types) {
+                for (String type : types) {
                     loadStat(type);
                 }
             }
-            
+
             calculateGlobal();
-            
+
             plugin.log("Loaded statistics for " + this.stats.size() + " types.");
-        }else {
+        } else {
             plugin.log("Not tracking statistics.");
         }
     }
